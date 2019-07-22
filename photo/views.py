eda0a5fdee038ac28bb93401318bd83da560a98b
muhindokiro <blaize1143@gmail.com>
2019-07-22 04:45:26 +0300

    defined tho home page home.html

--- photo/views.py
+++ photo/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http  import HttpResponse
 from .models import Photo
 
-# Create your views here.
+# Create your views here
 def home(request):    
     return render(request, 'home.html')
 
